9974648dae63591c988463778c7b3e698fb29fa4
Olubodun <bolowys33@gmail.com>
2026-03-27 12:32:20 +0100

    Update with js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const age = 100;
+
+if (age >= 18) {
+  console.log("You are eligible to vote");
+} else if (age > 12 && age < 18) {
+  console.log("You go soon start to dey vote, No worry");
+} else {
+  console.log("No near the polling units");
+}
